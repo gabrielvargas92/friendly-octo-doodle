56548a7f5f12c6a023f5cabe90cb277f0724554d
Maciel <gabrielm@trin.ca>
2021-12-28 00:04:57 -0300

    teste

---
+++ EasyCollection/Array/RemoveDuplicatesFromSortedArray.js
@@ -0,0 +1,25 @@
+RemoveDuplicatesFromSortedArray(['1', '1', '3', '4', '4', '4']);
+
+// In-place solution
+function RemoveDuplicatesFromSortedArray(nums) {
+    if (nums.length == 0) return 0;
+
+    var i = 0;
+
+    for (var j = 1; j < nums.length; j++) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+
+    return i + 1;
+}
+
+
+// 1 - [1, 1 ,3 ,4 ,4 ,4] ######### slow = 0  ########## fast = 1
+// 2 - [1, 3, 3 , 4, 4 ,4]
+// 3 - [1, 3, 4 , 4, 4 ,4]
+
+
+
